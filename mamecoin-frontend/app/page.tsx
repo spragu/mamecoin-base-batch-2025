cefc08f563dfa55c6df39ea056f6ffb3c49b76ea
spragu <sprague@cjs.guru>
2025-05-14 18:37:03 -0400

    whiny linter

--- mamecoin-frontend/app/page.tsx
+++ mamecoin-frontend/app/page.tsx
@@ -1,43 +1,14 @@
 'use client';
 
 import {
-  ConnectWallet,
-  Wallet,
-  WalletDropdown,
-  WalletDropdownLink,
-  WalletDropdownDisconnect,
   WalletAdvancedDefault,
-  WalletIsland,
 } from '@coinbase/onchainkit/wallet';
-import {
-  Address,
-  Avatar,
-  Name,
-  Identity,
-  EthBalance,
-} from '@coinbase/onchainkit/identity';
+
 import Image from 'next/image'
 import Subscribe from './components/Subscribe';
 import UsdcBalance from './components/UsdcBalance';
 import { FundCard } from '@coinbase/onchainkit/fund';
 import AppFlowModal from './components/AppFlowModal';
-
-// const components = [
-//   {
-//     name: 'Transaction',
-//     url: 'https://onchainkit.xyz/transaction/transaction',
-//   },
-//   { name: 'Swap', url: 'https://onchainkit.xyz/swap/swap' },
-//   { name: 'Checkout', url: 'https://onchainkit.xyz/checkout/checkout' },
-//   { name: 'Wallet', url: 'https://onchainkit.xyz/wallet/wallet' },
-//   { name: 'Identity', url: 'https://onchainkit.xyz/identity/identity' },
-// ];
-
-// const templates = [
-//   { name: 'NFT', url: 'https://github.com/coinbase/onchain-app-template' },
-//   { name: 'Commerce', url: 'https://github.com/coinbase/onchain-commerce-template'},
-//   { name: 'Fund', url: 'https://github.com/fakepixels/fund-component' },
-// ];
 
 export default function App() {
   return (
@@ -57,35 +28,9 @@ export default function App() {
           <div className="flex justify-center mb-6">
             <div className="wallet-container">
             <WalletAdvancedDefault />
-            {/* <WalletIsland /> */}
-                {/* <Wallet>
-                  <ConnectWallet>
-                    <Avatar className="h-6 w-6" />
-                    <Name />
-                  </ConnectWallet>
-                  <WalletDropdown>
-                    <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
-                      <Avatar />
-                      <Name />
-                      <Address />
-                      <EthBalance />
-                    </Identity>
-                    <WalletDropdownLink
-                      icon="wallet"
-                      href="https://keys.coinbase.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Wallet
-                    </WalletDropdownLink>
-                    <WalletDropdownDisconnect />
-                  </WalletDropdown>
-                </Wallet> */}
-          
             </div>
           </div>
 
-          
           <div className="flex justify-center mb-6">
             <UsdcBalance />            
           </div>
